a8a788fed173d1e27e3c3aa19a5bd4d2fc55c9fd
22cburrows <22cburrows@athenian.org>
2020-09-24 07:33:22 +0000

    Not done yet

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,16 +5,25 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int die = (int) (Math.random() * 6) + 1;
+        return die;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] sums = new int[11];
+        int[] totals = new int[n];
+        for (int i = 0; i < n; i++){
+            totals[i] = rollDie() + rollDie();
+        }
+        for (int i = 2; i < (totals.length + 2); i++){
+            if ()
+            for (int i2 = 0; )
+
+        }
+        return sums;
     }
 
 
